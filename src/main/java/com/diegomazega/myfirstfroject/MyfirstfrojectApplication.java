6873f6a7a68de31c46d6afedbf8c6a51cb0fe193
DiegoMazega <diego.mazega@live.com>
2020-07-13 14:53:10 -0300

    Product memory

--- src/main/java/com/diegomazega/myfirstfroject/MyfirstfrojectApplication.java
+++ src/main/java/com/diegomazega/myfirstfroject/MyfirstfrojectApplication.java
@@ -1,18 +1,25 @@
 package com.diegomazega.myfirstfroject;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.diegomazega.myfirstfroject.entities.Category;
+import com.diegomazega.myfirstfroject.entities.Product;
 import com.diegomazega.myfirstfroject.repositories.CategoryRepository;
+import com.diegomazega.myfirstfroject.repositories.ProductRepository;
 
 @SpringBootApplication
 public class MyfirstfrojectApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	
 	public static void main(String[] args) {
@@ -24,8 +31,21 @@ public class MyfirstfrojectApplication implements CommandLineRunner {
 		Category cat1 = new Category(1L, "Eletronics");
 		Category cat2 = new Category(2L, "Books");
 		
+		Product prod1 = new Product(1L, "TV", 2200.00, cat1);
+		Product prod2 = new Product(2L, "Designer Partners", 120.00, cat2);
+		Product prod3 = new Product(3L, "Xbox X", 3000.00, cat1);
+		Product prod4 = new Product(4L, "Docker", 100.00, cat2);
+		
+		cat1.getProducts().addAll(Arrays.asList(prod1,prod3));
+		cat2.getProducts().addAll(Arrays.asList(prod2, prod4));
+		
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
+		
+		productRepository.save(prod1);
+		productRepository.save(prod2);
+		productRepository.save(prod3);
+		productRepository.save(prod4);
 	}
 
 }
---
+++ src/main/java/com/diegomazega/myfirstfroject/resources/ProductResource.java
@@ -0,0 +1,34 @@
+package com.diegomazega.myfirstfroject.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.diegomazega.myfirstfroject.entities.Product;
+import com.diegomazega.myfirstfroject.repositories.ProductRepository;
+
+@RestController //Informa que é um tipo de requisição Rest
+@RequestMapping(value = "/products") // Informa a rota para usar ela
+public class ProductResource {
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@GetMapping //Diz que é uma requisição Get
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = productRepository.findAll();
+		return ResponseEntity.ok().body(list); // Retorna com codigo da familia 200 e já no formato Json
+	}
+	
+	@GetMapping(value = "/{id}") // requisição Get com um route parameter
+	public ResponseEntity<Product>findById(@PathVariable Long id){
+		Product prod = productRepository.findById(id);
+		return ResponseEntity.ok().body(prod);
+	}
+}
